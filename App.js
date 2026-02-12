0f5f0c1f3185294f3f1cbae0c81027ad9590f86c
VIctor Jehan de Santana <victorjehan.git@gmail.com>
2026-02-12 08:49:09 -0300

    Enhance App with image and user input fields
    Added image, input fields for email and password, and buttons for login and registration.

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  } from 'react-native';
+import { Button, TextInput } from 'react-native-web';
+import { SafeAreaView } from 'react-native-web';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Victor Jehan de Santana</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.imagem}
+        source={{
+          uri: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+        }}
+
+      />
+
+      <Text>Email</Text>
+      <TextInput
+        placeholder='Email'
+        style={styles.input}
+        keyboardType='email-address'
+      />
+      <Text>Senha</Text>
+      <TextInput
+        placeholder='Digite sua senha'
+        style={styles.input}
+        secureTextEntry
+      />
+      <SafeAreaView>
+        <Button title='Logar'
+          color='#67cf36'
+        ></Button>
+
+        <Button title='Cadastrar-se'
+          color='#67cf36'
+        ></Button>
+      </SafeAreaView>
+      <Text>Esqueceu a senha</Text>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#621ad6',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  imagem: {
+    height: 100,
+    width: 100,
+  }
 });
